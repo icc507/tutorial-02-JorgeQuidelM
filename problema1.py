c38fa43789f9ab9e1303047f2e3c76878104b8d2
JorgeQuidelM <163532224+JorgeQuidelM@users.noreply.github.com>
2024-03-22 09:28:29 -0300

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,22 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+t1 = tuple(input().split())
+t2 = tuple(input().split())
+
+t1_casted = []
+t2_casted = []
+
+for i in t1:
+    try:
+        t1_casted.append(eval(i))
+    except:
+        t1_casted.append(i)
+
+for i in t2:
+    try:
+        t2_casted.append(eval(i))
+    except:
+        t2_casted.append(i)
+
+print(tuple(t2_casted + t1_casted + t2_casted))
